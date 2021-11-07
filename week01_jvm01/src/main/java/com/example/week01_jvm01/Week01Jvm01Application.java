592deda1be01822b7bfabde9ac59513afdd3f239
zhukedong <zhukedong1996@163.com>
2021-11-07 16:59:52 +0800

    homework01

---
+++ week01_jvm01/src/main/java/com/example/week01_jvm01/Week01Jvm01Application.java
@@ -0,0 +1,13 @@
+package com.example.week01_jvm01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week01Jvm01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week01Jvm01Application.class, args);
+    }
+
+}
---
+++ week01_jvm01/src/test/java/com/example/week01_jvm01/Week01Jvm01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week01_jvm01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week01Jvm01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
